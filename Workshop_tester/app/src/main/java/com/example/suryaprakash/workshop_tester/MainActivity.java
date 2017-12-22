65a87a2dda64249ca37120ef7a4bf95799838256
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2017-12-22 09:55:54 +0530

    Added new function to update counter value and display it

--- Workshop_tester/app/src/main/java/com/example/suryaprakash/workshop_tester/MainActivity.java
+++ Workshop_tester/app/src/main/java/com/example/suryaprakash/workshop_tester/MainActivity.java
@@ -3,16 +3,40 @@ package com.example.suryaprakash.workshop_tester;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    Button b;
+    int count_value = 0;
+    TextView text_box;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_constraint);
+        setContentView(R.layout.activity_main_linear);
+
+
+        b = (Button) findViewById( R.id.counter );
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count_value++;
+                text_box = (TextView)findViewById(R.id.text_box);
+                text_box.setText(String.valueOf(count_value));
+            }
+        });
+
     }
 
+
+    // another function for generating toast message
     public void toaster(View view) {
         Toast.makeText(this,R.string.toast_message,Toast.LENGTH_LONG).show();
         return;
@@ -22,4 +46,5 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"French Toast ", Toast.LENGTH_SHORT).show();
         return;
     }
+
 }
