7515da4b2881611ae408dd782da9c890e241b062
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2017-12-23 15:46:21 +0530

    final commit

---
+++ RecyclerView/app/src/main/java/com/example/suryaprakash/recyclerview/City_Adapter.java
@@ -0,0 +1,57 @@
+package com.example.suryaprakash.recyclerview;
+
+
+import android.content.Context;
+import android.media.Image;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class City_Adapter extends RecyclerView.Adapter <City_Adapter.City_View_Holder> {
+
+    String[] cities;
+    int[] images;
+    MainActivity context;
+
+    public City_Adapter(MainActivity  mainActivity, String[] cities, int[] images) {
+        context = mainActivity;
+        this.cities = cities;
+        this.images = images;
+    }
+
+    @Override
+    public City_Adapter.City_View_Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater layout_inflater = LayoutInflater.from(context);
+        View final_image = layout_inflater.inflate(R.layout.row_layout, parent , false );
+        return new City_View_Holder( final_image );
+    }
+
+    @Override
+    public void onBindViewHolder(City_Adapter.City_View_Holder holder, int position) {
+
+        holder.image_view.setImageResource(images[position]);
+        holder.text_view.setText(cities[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length ;
+    }
+
+    public class City_View_Holder extends  RecyclerView.ViewHolder {
+
+        ImageView image_view ;
+        TextView text_view;
+
+        public City_View_Holder(View itemView) {
+            super(itemView);
+            image_view = (ImageView) itemView.findViewById(R.id.row_layout_imageview);
+            text_view  = (TextView) itemView.findViewById(R.id.row_layout_textview);
+        }
+    }
+}
--- RecyclerView/app/src/main/java/com/example/suryaprakash/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example/suryaprakash/recyclerview/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.suryaprakash.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recycler_view ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recycler_view = (RecyclerView) findViewById( R.id.recycler_view );
+
+        int[] images = new int[10];
+        for(int i=0;i<10;i++)
+            images[i] = R.mipmap.ic_launcher;
+
+        String[] cities = getResources().getStringArray(R.array.cities);
+
+        City_Adapter city_adapter= new City_Adapter( MainActivity.this, cities, images ) ;
+
+        recycler_view.setAdapter(city_adapter);
+        recycler_view.setLayoutManager ( new LinearLayoutManager(MainActivity.this) );
+
     }
 }
